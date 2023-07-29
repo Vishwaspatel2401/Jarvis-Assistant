d2864be9add830b55ff3a4870eb2673a18a62faa
VishwasPatel <Vishwaspatel2401@gmail.com>
2023-07-29 14:20:36 +0530

    commited main file

---
+++ jarvis.py
@@ -0,0 +1,98 @@
+import datetime
+import path
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import smtplib 
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('Voices',voices[0].id )
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+        print("Good Morning")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+        print("Good Afternoon")
+    else:
+        speak("Good Evening")
+        print("Good Evening")
+
+    speak("Greetings Sir; I am Jarvis; How may I help you today")
+
+def takeCommand():
+    #It takes microphone input from user and returns string output 
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source)    
+    
+    try: 
+        print("Recognizing..")
+        query = r.recognize_google(audio,language='en-in')
+        print(f"user says: {query}\n")
+
+        if query.lower() == 'quit':
+            return None
+        
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please..")
+        return 'none'
+    return query
+    
+if __name__=="__main__": 
+    wishMe() 
+    # while True:
+    if 1:    
+        query = takeCommand().lower()
+        
+        #logic for executing tasks based on query 
+        if "wikipedia" in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+        
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\Vishwas\\Music\\Music'
+            songs  = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir,songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f'Sir, the time is {strTime}')
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Vishwas\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'spotify' in query:
+            spotifypath = "C:\\Users\\Vishwas\\AppData\\Roaming\\Spotify\\Spotify.exe"
+            os.startfile(spotifypath)
+            
+    
